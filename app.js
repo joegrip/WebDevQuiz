e66fc28fa47e0637682f28e5e633cd310c343829
Jacob Harris <harrisjakef@gmail.com>
2020-09-24 15:33:28 -0400

    Feature 3 files

---
+++ app.js
@@ -0,0 +1,195 @@
+angular.module('app', ['ngMaterial', 'ngMessages']);
+
+
+
+/*--------------------- Home Component ---------------------*/
+const home = {
+    bindings: {
+        data: '<'
+    },
+    templateUrl: './home/home.html',
+    controller: 'HomeController'
+}
+
+// Home Component with Routing (Routed / Stateful)
+angular.module('app').component('home', home);
+
+// Home Controller with dependency injection using the array method
+angular.module('app').controller('HomeController', ['$scope', '$rootScope', 'handleAnswersService', 'handleDataService', function ($scope, $rootScope, handleAnswersService, handleDataService) {
+
+    const $ctrl = this;
+    $ctrl.hide = 0; //Controls quiz area visibility
+    $ctrl.done = 0; //Determines if the submit button is visible
+    $ctrl.prev = 0; //Controls the visibility of the previous button
+    $ctrl.currentQuestion = 1; //Counter to control flow of previous button
+    $scope.d = {
+        singleSelect: null,
+        q: [0, 0, 0],
+        showS: [1, 0, 0] //question visibility
+    };
+    handleDataService.getQuestions().then(function (result) {
+        $ctrl.questions = result.data.questions; //Use the handle service to obtain the list of questions
+
+    });
+    
+    $ctrl.record = function (qID) {
+        $scope.d.q[parseInt(qID, 10)] = $ctrl.questions[parseInt(qID, 10)].answers.indexOf($scope.d.singleSelect); //Record the user's answer in the q array
+        if (parseInt(qID, 10) < 2) { //Restrict the question array from growing past questions
+            $scope.d.showS[parseInt(qID, 10) + 1] = 1; //Make next question visible
+            $scope.d.showS[parseInt(qID, 10)] = 0; //Make previous question hidden
+        } else {
+            $scope.d.showS[parseInt(qID, 10)] = 0; //Make previous question hidden
+            $ctrl.done = 1; //The user has gone through all questions. Make submit button permanently visible
+        }
+        
+        $ctrl.currentQuestion += 1; //Move the flow counter forward
+        //Determine if the previous button should be visible. Mainly restricts the first question from having a previous button
+        $ctrl.prev = (parseInt(qID, 10) >= 0) ? 1 : 0;
+        $scope.d.singleSelect = 5; //Reset the singleSelect to a dead value
+
+    };
+
+
+    $ctrl.submit = function () {
+        handleAnswersService.setDone(); 
+        handleAnswersService.setAnswers($scope.d.q);
+        $ctrl.done = 0;
+        $ctrl.hide = 1;
+        $rootScope.$emit("qDone", "");
+
+
+    };
+    
+    $ctrl.previous = function(){
+        //Only set the current element to 0 if it's within the length of the showS array
+        if($ctrl.currentQuestion < 4) $scope.d.showS[$ctrl.currentQuestion - 1] = 0;
+        $scope.d.showS[$ctrl.currentQuestion - 2] = 1; //Make the previous question visible
+        $ctrl.currentQuestion -= 1; //Adjust the flow counter
+        $ctrl.prev = ($ctrl.currentQuestion > 1) ? 1 : 0; //Determine if the previous button should be visible.
+    };
+
+    $ctrl.getPrev = function (){
+        return $ctrl.prev;
+    };
+    
+    $ctrl.getDone = function () {
+        return $ctrl.done;
+    };
+    $ctrl.showSelect = function (qID) {
+        return $scope.d.showS[parseInt(qID, 10)];
+    };
+
+
+}]);
+
+/*--------------------- Result Component ---------------------*/
+const result = {
+    bindings: {
+        answers: '<'
+    },
+    templateUrl: './result/result.html',
+    controller: 'ResultController'
+}
+
+// Home Component with Routing (Routed / Stateful)
+angular.module('app').component('result', result);
+
+// Home Controller with dependency injection using the array method
+angular.module('app').controller('ResultController', ['$scope', '$rootScope', 'handleAnswersService', 'handleResultService', function ($scope, $rootScope, handleAnswersService, handleResultService) {
+
+    const $ctrl = this;
+    $ctrl.show = 0;
+
+
+    $rootScope.$on("qDone", function (evt, arg) {
+        $ctrl.answers = handleAnswersService.getAnswers();
+        $ctrl.show = 1;
+        $ctrl.result = "You answered " + $ctrl.answers[0].toString();
+        $ctrl.score = 0;
+        //Add up all the elements of the user answer arary
+        $ctrl.answers.forEach(function(oneElem){
+            $ctrl.score += oneElem;
+        });
+    
+        //Get the data that corresponds to the user's score
+        handleResultService.getResultOptions().then(function (result){
+            $ctrl.resultMessage = result.data[$ctrl.score].message;
+            $ctrl.resultIMG = result.data[$ctrl.score].img;
+        });
+        
+    });
+    
+}]);
+
+
+
+
+function handleDataService($http) {
+
+    this.getQuestions = getQuestions;
+    this.getSubmitted = getSubmitted;
+    this.wasSubmitted = wasSubmitted;
+    this.submitted = 0;
+
+    function getSubmitted() {
+        return this.submitted;
+    }
+
+    function wasSubmitted() {
+        this.submitted = 1;
+    }
+    //Get the questions from the JSON
+    function getQuestions() {
+        return $http({
+            method: 'GET',
+            url: 'questions.json'
+        })
+    }
+
+}
+angular.module('app').service('handleDataService', handleDataService);
+
+
+
+function handleAnswersService() {
+
+    this.getAnswers = getAnswers;
+    this.setAnswers = setAnswers;
+    this.getDone = getDone;
+    this.setDone = setDone;
+    this.answers = [0, 0, 0];
+    this.done = 0;
+
+    function getAnswers() {
+        return this.answers;
+    }
+
+    function setAnswers(ans) {
+        this.answers = ans;
+    }
+
+    function getDone() {
+        return this.done;
+    }
+
+    function setDone() {
+        this.done = 1;
+    }
+
+
+}
+angular.module('app').service('handleAnswersService', handleAnswersService);
+
+function handleResultService($http){
+    
+    this.getResultOptions = getResultOptions;
+    
+    function getResultOptions(){
+        return $http({
+            method: 'GET',
+            url: 'results.json'
+        })
+    }
+    
+}
+angular.module('app').service('handleResultService', handleResultService);
